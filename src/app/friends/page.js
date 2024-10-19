617f1a440932a10f8ed2c4c5255292a589a3d9db
Kshitij Kochhar <kshitij@ctfguide.com>
2024-10-18 21:59:05 -0700

    [feat] rough layout done

---
+++ src/app/friends/page.js
@@ -0,0 +1,109 @@
+"use client";
+
+import { useState } from "react";
+import {
+	Dialog,
+	DialogBackdrop,
+	DialogPanel,
+	TransitionChild,
+} from "@headlessui/react";
+import {
+	Bars3Icon,
+	CalendarIcon,
+	ChartPieIcon,
+	DocumentDuplicateIcon,
+	FolderIcon,
+	HomeIcon,
+	UsersIcon,
+	XMarkIcon,
+} from "@heroicons/react/24/outline";
+
+const navigation = [
+	{ name: "Home", href: "/home", icon: HomeIcon, current: false },
+	{ name: "Family", href: "/family", icon: UsersIcon, current: false },
+	{ name: "Friends", href: "/friends", icon: FolderIcon, current: true },
+];
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
+
+export default function Friends() {
+	const [sidebarOpen, setSidebarOpen] = useState(false);
+
+	return (
+		<>
+			<div>
+				{/* Desktop Sidebar */}
+				<div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
+					<div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+						<div className="flex h-16 items-center">
+							<img
+								alt="Your Company"
+								src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+								className="h-8 w-auto"
+							/>
+						</div>
+						<nav className="flex flex-1 flex-col">
+							<ul role="list" className="flex flex-1 flex-col gap-y-7">
+								<li>
+									<ul role="list" className="-mx-2 space-y-1">
+										{navigation.map((item) => (
+											<li key={item.name}>
+												<a
+													href={item.href}
+													className={classNames(
+														item.current
+															? "bg-gray-50 text-indigo-600"
+															: "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+														"group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+													)}
+												>
+													<item.icon
+														aria-hidden="true"
+														className={classNames(
+															item.current
+																? "text-indigo-600"
+																: "text-gray-400 group-hover:text-indigo-600",
+															"h-6 w-6 shrink-0"
+														)}
+													/>
+													{item.name}
+												</a>
+											</li>
+										))}
+									</ul>
+								</li>
+
+								<li className="-mx-6 mt-auto">
+									<a
+										href="#"
+										className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
+									>
+										<img
+											alt=""
+											src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+											className="h-8 w-8 rounded-full bg-gray-50"
+										/>
+										<span className="sr-only">Your profile</span>
+										<span aria-hidden="true">Tom Cook</span>
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+
+				<main className="">
+					<div className="">
+						<div className="px-4 py-10">
+              {/* Main area */}
+            </div>
+					</div>
+				</main>
+
+				<div className="fixed inset-y-0 right-96 hidden min-w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block"></div>
+			</div>
+		</>
+	);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,80 @@
-import Image from "next/image";
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
 
-export default function Home() {
+export default function Example() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="relative isolate overflow-hidden bg-white">
+      <svg
+        aria-hidden="true"
+        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+      >
+        <defs>
+          <pattern
+            x="50%"
+            y={-1}
+            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+            width={200}
+            height={200}
+            patternUnits="userSpaceOnUse"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <rect fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" width="100%" height="100%" strokeWidth={0} />
+      </svg>
+      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
+          <Image
+            alt="Your Company"
+            src="/logos/mark.svg?color=indigo&shade=600" // Changed src to a local path
+            width={44}
+            height={11}
+            className="h-11"
+          />
+          <div className="mt-24 sm:mt-32 lg:mt-16">
+            <a href="#" className="inline-flex space-x-6">
+              <span className="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10">
+                What&apos;s new
+              </span>
+              <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
+                <span>Just shipped v1.0</span>
+                <ChevronRightIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
+              </span>
+            </a>
+          </div>
+          <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Deploy to the cloud with confidence
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+            fugiat veniam occaecat fugiat aliqua.
+          </p>
+          <div className="mt-10 flex items-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Get started
+            </a>
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              Learn more <span aria-hidden="true">→</span>
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image
+                alt="App screenshot"
+                src="/component-images/project-app-screenshot.png" // Changed src to a local path
+                width={2432}
+                height={1442}
+                className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
